b23f85f96f952ca166c98e1c3e329f28fc4c7a37
Ahashanahmed2 <83818778+Ahashanahmed2@users.noreply.github.com>
2026-05-18 14:15:22 +0800

    Update models.py

--- models.py
+++ models.py
@@ -3,8 +3,9 @@ from typing import Optional
 
 class PEGYInput(BaseModel):
     symbol: str = Field(..., min_length=1, max_length=20, description="Ticker Symbol")
-    eps: float = Field(..., gt=0, description="Earnings Per Share (টাকায়)")
+    eps: float = Field(..., gt=0, description="Current EPS (টাকায়)")
+    eps_old: Optional[float] = Field(None, description="Old EPS 3 Years Ago (টাকায়)")
     eps_period: str = Field(..., description="annual or quarterly")
     dividend_yield: float = Field(..., ge=0, description="Dividend Yield (%)")
-    eps_growth: Optional[float] = Field(None, ge=0, description="EPS Growth Rate 3Yr (%)")
+    eps_growth: Optional[float] = Field(None, description="EPS Growth Rate 3Yr (%)")
     current_price: float = Field(..., gt=0, description="Current Stock Price (টাকায়)")
